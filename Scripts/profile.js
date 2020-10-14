1035bcc844c479ebbd3a24cc1d04feeadfa55251
Ashutosh Kumar <crownashutosh01@gmail.com>
2020-10-14 08:03:35 +0530

    Alpha 5
    Imporved responsiveness

--- Scripts/profile.js
+++ Scripts/profile.js
@@ -1,6 +1,5 @@
 onload = () => {
   header_animation()
-  aside_expand_collapse()
   menu_button()
 }
 
@@ -27,51 +26,6 @@ const header_animation = () => {
     header_sticky = false
     header.removeAttribute(`style`)
   }
-}
-
-
-const aside_expand_collapse = () => {
-  const timeline_title = document.querySelector(`main aside.timeline span.timeline-title`)
-  const blog_title = document.querySelector(`main aside.latest-blogs span.blog-title`)
-
-  //marks
-  const timeline_mark = document.querySelector(`main aside.timeline span.mark`)
-  const blog_mark = document.querySelector(`main aside.latest-blogs span.mark`)
-
-  //links
-  const timeline_link = document.querySelectorAll(`main aside.timeline a`)
-  const blog_link = document.querySelectorAll(`main aside.latest-blogs a`)
-
-  timeline_title.addEventListener(`click`, () => {
-    if (window.innerWidth < 720){
-      timeline_link.forEach(link => {
-        link.classList.toggle(`visible`)
-        if (link.classList.contains(`visible`)) {
-          link.style.display = `block`
-          link.style.height = `fit-content`
-          timeline_mark.style = `transform: rotate(180deg)`
-        } else {
-          link.removeAttribute(`style`)
-          timeline_mark.style = `transform: rotate(0deg)`
-        }
-      });
-    }
-  })
-  blog_title.addEventListener(`click`, () => {
-    if (window.innerWidth < 720){
-      blog_link.forEach(link => {
-        link.classList.toggle(`visible`)
-        if (link.classList.contains(`visible`)) {
-          link.style.display = `block`
-          link.style.height = `fit-content`
-          blog_mark.style = `transform: rotate(180deg)`
-        } else {
-          link.removeAttribute(`style`)
-          blog_mark.style = `transform: rotate(0deg)`
-        }
-      });
-    }
-  })
 }
 
 const menu_button = () => {
