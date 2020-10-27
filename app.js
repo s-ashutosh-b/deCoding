7fbf1e7445a6c725d128103b01c835c984721178
Ashutosh Kumar <crownashutosh01@gmail.com>
2020-10-27 11:18:09 +0530

    Database Connected
    There was a firewall issue.

--- app.js
+++ app.js
@@ -2,20 +2,33 @@
 
 require(`dotenv`).config()
 const express = require(`express`)
-const app = express()
 const router = require(`./routes/routes`)
+const mongoose = require(`mongoose`)
+
+const app = express()
 const PORT = process.env.PORT
 
 // Set up view Engine
 app.set(`view engine`, `ejs`)
+
+// Connect Database
+const USER_NAME = process.env.USER_NAME
+const PASSWORD = process.env.PASSWORD
+const DATABASE_NAME = process.env.DATABASE_NAME
+//console.log(USER_NAME, PASSWORD, DATABASE_NAME)
+
+mongoose.connect(`mongodb://${USER_NAME}:${PASSWORD}@cluster0-shard-00-00.pxs2w.mongodb.net:27017,cluster0-shard-00-01.pxs2w.mongodb.net:27017,cluster0-shard-00-02.pxs2w.mongodb.net:27017/${DATABASE_NAME}?ssl=true&replicaSet=atlas-14nipb-shard-0&authSource=admin&retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true },)
+  .then((result) => {
+    console.log(`Connected to database: ${DATABASE_NAME}`)
+    // Server
+    app.listen(PORT, () => {
+      console.log(`Listining at http://localhost:${PORT}`)
+    })
+  })
+  .catch(err => console.log(`__error__`, err))
 
 // Set up public folder
 app.use(express.static(__dirname + '/public/'))
 
 // Routs
 app.use(router)
-
-// Server
-app.listen(PORT, () => {
-  console.log(`Listining at http://localhost:${PORT}`)
-})
--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,32 @@
 const express = require(`express`)
 const blogRoutes = require(`./blogRoutes`)
 const userRoutes = require(`./userRoutes`)
+const Blog = require(`../models/blog`)
 
 const router = express.Router()
 
 // Routes
 router.get(`/`, (req, res) => {
   res.render(`index`)
+})
+
+router.get(`/new-blog`, (req, res)=>{
+  const blog = new Blog ({
+    title: `blog title 2`,
+    subtitle: `blog Subtitle`,
+    body: `Hey~ This is new blog body...`
+  })
+  blog.save()
+    .then((result)=>{
+      res.send(result)
+    })
+    .catch(err => {
+      res.send(err)
+    })
+})
+
+router.get(`/all-blogs`, (req, res)=>{
+  Blog.find().then(result =>res.send(result)).catch(err => res.send(err))
 })
 
 router.get(`/login`, (req, res) => {
